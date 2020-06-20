f1ed441206a55621d356d769c7e0c3937b4f15d3
Maksym Polinka <Polinka7max@gmail.com>
2020-06-20 10:55:30 +0300

    took apart data and event binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ComponentStillLoadingError } from '@angular/compiler/src/private_import_core';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,18 @@ export class HomeComponent implements OnInit {
   homeTitle = "Welcome to the ninja directory";
   myString = "I love chicken";
   myBoolean = true;
+  @Input() ninja;
+  @Output() onYell = new EventEmitter();
+
+  alertMe(val) {
+    alert(val);
+  }
+
+  fireYellEvent(e) {
+    console.log("fireYellEvent fired"); 
+    this.onYell.emit(e);
+  }
+
   constructor() { }
 
   ngOnInit() {
